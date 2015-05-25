6272a5955258ae337e3d302751c998e34b325f7c
Karl Brown <karl@thekarlbrown.com>
2015-05-25 17:07:11 -0400

    Documented Test Cases
    - Comments Added to JUnit section

--- src/SwingTestCase.java
+++ src/SwingTestCase.java
@@ -2,7 +2,7 @@ import junit.framework.TestCase;
 
 /**
  * Created by TheKarlBrown on 5/25/15.
- *
+ * Test cases for the JUnit Section. Zero and Regular
  */
 public class SwingTestCase extends TestCase {
     private TestableSwing testFrame;
@@ -22,12 +22,15 @@ public class SwingTestCase extends TestCase {
         }
     }
 
+    //Tests for a typical result
     public void testAreaCalculationOne(){
         testFrame.getRadiusTextField().setText("5");
         testFrame.getHeightTextField().setText("10");
         testFrame.convertButtonActionPerformed();
         assertEquals(785.398163397, Double.parseDouble(testFrame.getVolumeLabel().getText().substring(20,33)),.000000001);
     }
+
+    //Checks that Zero is handled properly
     public void testAreaCalculationZero(){
         testFrame.getRadiusTextField().setText("5");
         testFrame.getHeightTextField().setText("0");
